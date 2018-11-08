8595ac8b56bdd1ad53f7439d37b47394928d907f
MBorgoglio <42551410+MBorgoglio@users.noreply.github.com>
2018-11-08 17:47:43 -0300

    Update Vistas.sql

--- Vistas.sql
+++ Vistas.sql
@@ -1,1 +1,6 @@
-
+1) CREATE VIEW P_Dpto21 AS SELECT * FROM Profesor WHERE CodDpto = 21;
+2) No es factible insertar un cod 22 porque la vista es 21. 
+   CREATE VIEW P_Dpto22 AS SELECT * FROM Profesor WHERE CodDpto=22; 
+3)CREATE VIEW Precio_Matricula AS SELECT (Nombre), (Creditos) * 15 AS Matricula FROM Asignatura;
+  SELECT * FROM Precio_Matricula;
+4) SELECT (CodAsig) AS Asignatura, (CodGrupo) AS Grupo FROM Grupo WHERE (SELECT (CodAsig) 
